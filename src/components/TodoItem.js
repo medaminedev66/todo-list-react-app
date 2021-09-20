7c947fbe8921eb0b2b9220b440276e4ed9ac56af
Amine Smahi <md.amine.smahi@gmail.com>
2021-09-20 15:26:54 +0100

    add completedStyle to title span

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import styles from './TodoItem.module.css';
 class TodoItem extends React.Component {
+  completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
   render() {
+    const { id, title, completed } = this.props.todo;
     return (
       <li className={styles.item}>
         <input
           className={styles.checkbox}
           type="checkbox"
-          checked={this.props.todo.completed}
+          checked={completed}
           onChange={() => {
-            this.props.handelChangeProp(this.props.todo.id);
+            this.props.handelChangeProp(id);
           }}
         />
-        {this.props.todo.title}
+        <span style={completed ? this.completedStyle : null}>{title}</span>
         <button
           onClick={() => {
-            this.props.deleteTodoProp(this.props.todo.id);
+            this.props.deleteTodoProp(id);
           }}
         >
           Delete
